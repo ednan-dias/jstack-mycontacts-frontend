10293a21185de38511106930d98f3fcc4bcae426
Ednan Dias <ednan.741@gmail.com>
2022-12-07 22:39:26 -0300

    add some components

---
+++ src/pages/Home/index.js
@@ -0,0 +1,93 @@
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  Container, Header, ListContainer, InputSearchContainer,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+export default function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquise pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 Contatos</strong>
+        <Link to="/new">Novo contato</Link>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Ednan Dias</strong>
+              <small>instagram</small>
+            </div>
+            <span>ednan.741@gmail.com</span>
+            <span>(17)99126-2596</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Ednan Dias</strong>
+              <small>instagram</small>
+            </div>
+            <span>ednan.741@gmail.com</span>
+            <span>(17)99126-2596</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Ednan Dias</strong>
+              <small>instagram</small>
+            </div>
+            <span>ednan.741@gmail.com</span>
+            <span>(17)99126-2596</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
+}
