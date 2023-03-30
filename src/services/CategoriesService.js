c9101f74874aee2d99491af412774f9c68676542
Ednan Dias <ednan.741@gmail.com>
2023-03-30 13:47:46 -0300

    add env

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -3,12 +3,7 @@ import HttpClient from './utils/HttpClient';
 
 class CategoriesService {
   constructor() {
-    const baseUrl = {
-      dev: process.env.REACT_APP_DEV_BASE_URL,
-      prod: process.env.REACT_APP_PROD_BASE_URL,
-    };
-
-    this.httpClient = new HttpClient(process.env.NODE_ENV === 'production' ? baseUrl.prod : baseUrl.dev);
+    this.httpClient = new HttpClient(process.env.REACT_APP_BASE_URL);
   }
 
   async listCategories(signal) {
